75c108db2760b521a2ef14d0741cf818c43e97d5
514InParadise <63651906+Tomasinity@users.noreply.github.com>
2023-12-23 21:41:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express()
 
 app.post('/', (req, res) => {
   // 在这里可以进行其他处理，例如返回响应给客户端
-  res.send('GET request received with key1=' + ' and key2=');
+  const key1 = req.body.key1;
+  res.send('GET request received with key1=' + key1);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome');
 });
 
 const port = process.env.PORT || 80
