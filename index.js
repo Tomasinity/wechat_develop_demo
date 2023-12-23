72122b8d5275cb8a31e83e5f7653d271ce6bacea
514InParadise <63651906+Tomasinity@users.noreply.github.com>
2023-12-23 20:29:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('welcome to use Wechat cloud reporsity!');
 }) 
 
+app.post('/', (req, res) => {
+    console.log(req.body);
+})
+
 const port = process.env.PORT || 80
 app.listen(port, () => {
     console.log('服务启动成功，端口：', port)
