ebcf235f4e2c801a967fdce344214bbc090e1470
514InParadise <63651906+Tomasinity@users.noreply.github.com>
2023-12-23 21:30:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/api', (req, res) => {
-  // 获取查询字符串参数
-  const key1 = req.query.key1;
-  const key2 = req.query.key2;
-
-  // 在控制台中打印参数
-  console.log('key1:', key1);
-  console.log('key2:', key2);
-
+app.get('/', (req, res) => {
   // 在这里可以进行其他处理，例如返回响应给客户端
-  res.send('GET request received with key1=' + key1 + ' and key2=' + key2);
+  res.send('GET request received with key1=' + ' and key2=');
 });
 
 const port = process.env.PORT || 80
